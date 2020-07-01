22ebd3377c6b553a2b39832105c787bc36488dac
CalmCat <747458467@qq.com>
2020-07-01 09:25:02 +0800

    init project

---
+++ JavaEE_SummerProject/target/JavaEE_SummerProject/js/myjs.js
@@ -0,0 +1,56 @@
+// JavaScript Document
+
+
+	function sign_in() {
+			alert("已点击按钮");
+			let Username = $.trim($("#Username").val());
+			let Password = $.trim($("#Password").val());
+			
+          	
+
+
+		$.ajax({
+			type:"POST", // 使用post方式
+
+			// 志愿者报名
+			url:"http://192.168.14.240:8888/login",
+			//contentType:"application/json",
+
+
+			data: {data:JSON.stringify({
+				"status": "0",
+				"message": "volunteerSend",
+					"content": [
+						{
+							"id": "UserName",
+							"value":Username
+						},
+						{
+							"id": "Password",
+							"value":Password
+						},
+						
+					]
+				
+			})},
+
+
+			dataType:"json",
+
+			success: function(result){
+				// 请求成功后的操作
+				if (result.successValue == "1"){
+					alert("登陆成功");
+					location.href = "/index.html";
+				}
+				else{
+					alert("用户名或密码错误");
+				}
+			},
+
+			error: function(XMLHttpRequest, textStatus, errorThrown) {
+				alert("插入数据失败,请检查数据库链接");
+				//alert(textStatus);
+			}
+		});
+	}
