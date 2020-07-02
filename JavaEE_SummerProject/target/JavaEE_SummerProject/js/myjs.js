e133017484839ded38b08f307583c0da594cac0b
CalmCat <747458467@qq.com>
2020-07-02 10:17:47 +0800

    更新跳转

--- JavaEE_SummerProject/target/JavaEE_SummerProject/js/myjs.js
+++ JavaEE_SummerProject/target/JavaEE_SummerProject/js/myjs.js
@@ -1,57 +1,61 @@
 // JavaScript Document
 
 
-	function sign_in() {
-			alert("已点击按钮");
-			let Username = $.trim($("#Username").val());
-			let Password = $.trim($("#Password").val());
+function sign_in() {
+		alert("已点击按钮");
+		let Username = $.trim($("#Username").val());
+		let Password = $.trim($("#Password").val());
+		var curPageUrl = window.document.location.href;
+		var rootPath = curPageUrl.split("//")[0] + curPageUrl.split("//")[1].split("/")[0] + "/" + curPageUrl.split("//")[1].split("/")[1];
 
 
 
 
-		$.ajax({
-			type:"POST", // 使用post方式
+	$.ajax({
+		type:"POST", // 使用post方式
 
-			// 志愿者报名
-			url:"/login",
-			contentType:"application/json",
-
-
-			data:JSON.stringify({
-				"status": "0",
-				"message": "volunteerSend",
-					"content": [
-						{
-							"id": "UserName",
-							"value":Username
-						},
-						{
-							"id": "Password",
-							"value":Password
-						},
-
-					]
-
-			}),
+		// 志愿者报名
+		url:"/login",
+		contentType:"application/json",
 
 
-			dataType:"json",
-			async:false,
-			success: function(result){
-				// 请求成功后的操作
-				if (result.successvalue == "1"){
-					alert("登陆成功");
-					location.href = "index.html";
-				}
-				else{
-					alert("用户名或密码错误");
-					//location.href = "pages-login.html";
-				}
-			},
+		data:JSON.stringify({
+			"status": "0",
+			"message": "volunteerSend",
+				"content": [
+					{
+						"id": "UserName",
+						"value":Username
+					},
+					{
+						"id": "Password",
+						"value":Password
+					},
 
-			error: function(XMLHttpRequest, textStatus, errorThrown) {
-				alert("插入数据失败,请检查数据库链接");
-				//alert(textStatus);
+				]
+
+		}),
+
+
+		dataType:"json",
+		async:false,
+		success: function(result){
+			// 请求成功后的操作
+			if (result.successvalue == "1"){
+				alert("登陆成功");
+				alert(rootPath);
+				alert("2");
+				window.location.href = "index.html";
 			}
-		});
-	}
+			else{
+				alert("用户名或密码错误");
+				//location.href = "pages-login.html";
+			}
+		},
+
+		error: function(XMLHttpRequest, textStatus, errorThrown) {
+			alert("插入数据失败,请检查数据库链接");
+			//alert(textStatus);
+		}
+	});
+}
