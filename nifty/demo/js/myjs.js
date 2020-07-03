4aa1b9793f6e816bfe3ad838d7ca278ac9fda347
zhangyizhuo24 <zhangyizhuo1124@163.com>
2020-07-03 15:40:31 +0800

    update info

--- nifty/demo/js/myjs.js
+++ nifty/demo/js/myjs.js
@@ -3,8 +3,8 @@
 
 	function sign_in() {
 			alert("已点击按钮");
-			let Username = $.trim($("#Username").val());
-			let Password = $.trim($("#Password").val());
+			let user_name = $.trim($("#Username").val());
+			let pass_word = $.trim($("#Password").val());
 
 
 
@@ -18,33 +18,25 @@
 
 
 			data:JSON.stringify({
-				"status": "0",
-				"message": "volunteerSend",
-					"content": [
-						{
-							"id": "UserName",
-							"value":Username
-						},
-						{
-							"id": "Password",
-							"value":Password
-						},
+                "status": "0",
+                "message": "login",
+                "user_name": user_name,
+                "pass_word": pass_word,
 
-					]
-
-			}),
-
+                
+            }),
+			
 
 			dataType:"json",
 			async:false,
 			success: function(result){
 				// 请求成功后的操作
-				if (result.successvalue == "1"){
+				if (result.success_value == "1"){
 					alert("登陆成功");
 					location.href = "index.html";
 				}
 				else{
-					alert("用户名或密码错误");
+					alert(fail_content);
 					//location.href = "pages-login.html";
 				}
 			},
@@ -55,3 +47,71 @@
 			}
 		});
 	}
+
+
+	$(document).ready(function(){ 
+			$("a.list-group-item").click(function(e){
+					e.preventDefault();
+					var _this = $(this);
+					var selector = _this.attr("href");
+					var _siblings = _this.siblings(".selected");
+					var id = _siblings.attr("href");
+
+					_siblings.removeClass("selected");
+					$( id ).removeClass("testing");
+
+					_this.addClass("selected");
+					$(selector).addClass("testing");
+
+				});
+			});
+
+
+
+	function recent_info(){
+		
+		$.ajax({
+			type:"POST", // 使用post方式
+
+			url:"/UpcomingIssue",
+			contentType:"application/json",
+			
+			data: JSON.stringify({
+                "status": "0",
+                "message": "upcoming_issue",
+                
+            }),
+			
+			
+			dataType:"json",
+			async:false,
+			success: function(result){
+				if (result.success_value == "1"){
+				
+				// 请求成功后的操作
+					var dataObj = result,
+					var info_data = "",
+                	$.each(dataObj, function(index, item){
+						var rec_info="";
+						rec_info += "<td>"+item.id+"</td>";
+                    	rec_info += "<td>"+item.value+"</td>";
+						rec_info += "<td>"+item.info_id_path+"</td>";
+  						rec_info += "<td>"+item.course+"</td>";
+						rec_info += "<td>"+item.datetime+"</td>";
+						rec_info += "<td>"+item.content+"</td>";
+						info_data += "<tr>"+rec_info+"</tr>" ;
+				});
+					$("#recent-info-data").html(info_data);
+	 				
+	
+			}
+				else{
+					alert(fail_content);
+					//location.href = "pages-login.html";
+				}
+		  },
+
+		});
+		}
+		
+
