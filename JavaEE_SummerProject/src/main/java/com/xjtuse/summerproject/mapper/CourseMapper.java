0f38ab7c158f086d3ece8ffe3a52636a8898b8a7
CalmCat <747458467@qq.com>
2020-07-08 15:01:22 +0800

    update getCoursePanel mapper

--- JavaEE_SummerProject/src/main/java/com/xjtuse/summerproject/mapper/CourseMapper.java
+++ JavaEE_SummerProject/src/main/java/com/xjtuse/summerproject/mapper/CourseMapper.java
@@ -1,6 +1,8 @@
 package com.xjtuse.summerproject.mapper;
 
+import com.xjtuse.summerproject.entity.Course;
 import com.xjtuse.summerproject.entity.CourseContent;
+import com.xjtuse.summerproject.entity.CoursePanel;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
@@ -9,5 +11,9 @@ public interface CourseMapper {
     //查询课程对应的course_content_table内容
     @Select("SELECT * FROM ${str} WHERE issue_type != 'homework' AND issue_type != 'examination'" )
     List<CourseContent> findUpcomingIssue(String str);
+
+    @Select("SELECT * FROM course_${course_id}_design WHERE design_id NOT IN (\"announcement\", \"discussion\", \"examination\", \"homework\", \"resource\")")
+    List<CoursePanel> getCoursePanel(String course_id);
+
 
 }
